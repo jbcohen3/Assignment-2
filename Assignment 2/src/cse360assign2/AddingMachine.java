410be3127e7529c5f4860c8f242ed06e2a14f1a9
Jacob Cohen <Jacob Cohen@DESKTOP-UMP18AI>
2019-10-07 09:04:17 -0700

    Added functionality to AddingMachine.

--- Assignment 2/src/cse360assign2/AddingMachine.java
+++ Assignment 2/src/cse360assign2/AddingMachine.java
@@ -10,15 +10,18 @@ package cse360assign2;
 /**
  * class AddingMachine to add to or subtract from a total initially set to 0
  */
-public class AddingMachine {
-
+public class AddingMachine
+{
 	private int total;
+	private String format;
 	
 	/**
 	 * constructor to initialize a new AddingMachine
 	 */
-	public AddingMachine () {
+	public AddingMachine ()
+	{
 		total = 0;  // not needed - included for clarity
+		format = "0";
 	}
 	
 	/**
@@ -27,8 +30,9 @@ public class AddingMachine {
 	 * 
 	 * @return total
 	 */
-	public int getTotal () {
-		return 0;
+	public int getTotal ()
+	{
+		return total;
 	}
 	
 	/**
@@ -36,8 +40,10 @@ public class AddingMachine {
 	 * 
 	 * @param value to add to this AddingMachine
 	 */
-	public void add (int value) {
-		
+	public void add (int value)
+	{
+		total += value;
+		format += " + " + value;
 	}
 	
 	/**
@@ -45,8 +51,10 @@ public class AddingMachine {
 	 * 
 	 * @param value to subtract from this AddingMachine
 	 */
-	public void subtract (int value) {
-		
+	public void subtract (int value)
+	{
+		total -= value;
+		format += " - " + value;
 	}
 		
 	/**
@@ -55,14 +63,17 @@ public class AddingMachine {
 	 * 
 	 * @return format
 	 */
-	public String toString () {
-		return "";
+	public String toString ()
+	{
+		return format;
 	}
 	
 	/**
-	 * resets this AddingMachine by setting the total to 0 and setting the format to ""
+	 * resets this AddingMachine by setting the total to 0 and setting the format to "0"
 	 */
-	public void clear() {
-	
+	public void clear()
+	{
+		total = 0;
+		format = "0";
 	}
 }
